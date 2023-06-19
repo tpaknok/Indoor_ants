87cf656affade5283c4aa3f239d7ddb21655cca8
tpaknok <tpaknok@gmail.com>
2023-06-19 13:36:59 -0400

    Changes 19062023

--- Score.R
+++ Score.R
@@ -14,6 +14,7 @@ record_score <- sp_country %>% group_by(total_score) %>% summarise("2°C Alien" 
                                                                    "4°C Invasive" = sum(proj_diff_4C[invasive=="Invasive"]))
 
 record_score <- record_score %>% pivot_longer(!total_score)
+record_score %>% group_by(name) %>% summarize(percent = value[total_score==5]/sum(value)*100)
 
 p6 <- ggplot(data=record_score)+
   geom_point(aes(x=total_score,y=value))+
@@ -26,6 +27,46 @@ p6 <- ggplot(data=record_score)+
 
 plot(p6)
 ggsave("Fig6.tiff",dpi=800,width=16.8,height=16.8,units="cm",compression="lzw",bg="white")
+
+country_record_score <- sp_country %>% group_by(total_score,Final_country) %>% summarise("2°C Alien" = sum(proj_diff_2C),
+                                                                           "4°C Alien" = sum(proj_diff_4C),
+                                                                           "2°C Invasive" = sum(proj_diff_2C[invasive == "Invasive"]),
+                                                                           "4°C Invasive" = sum(proj_diff_4C[invasive=="Invasive"]))
+
+sum_stat <- country_record_score %>% group_by(total_score) %>% summarise(n= n(),
+                                                                   mean_2C_alien = mean(`2°C Alien`),
+                                                                   sd_2C_alien = sd(`2°C Alien`),
+                                                                   mean_4C_alien = mean(`4°C Alien`),
+                                                                   sd_4C_alien = sd(`4°C Alien`),
+                                                                   mean_2C_invasive = mean(`2°C Invasive`),
+                                                                   sd_2C_invasive = sd(`2°C Invasive`),
+                                                                   mean_4C_invasive = mean(`4°C Invasive`),
+                                                                   sd_4C_invasive = sd(`4°C Invasive`),)
+
+
+country_record_score_long <- na.omit(country_record_score) %>% pivot_longer(cols=-c(Final_country,total_score),names_to="scenario")
+
+pS1 <- ggplot(data=country_record_score_long)+
+  geom_boxplot(aes(x=total_score,y=value,group=total_score))+
+  #geom_jitter(aes(x=total_score,y=value),width=0.1)+
+  xlab("Total score (0-5)")+
+  ylab("Effects of climate change")+
+  facet_wrap(~scenario,scales="free")+
+  theme_classic()+
+  theme(legend.position="bw")
+
+plot(pS1)
+ggsave("FigS1.tiff",dpi=800,width=16.8,height=16.8,units="cm",compression="lzw",bg="white")
+
+library(glmmTMB)
+library(performance)
+r2(glmmTMB(`2°C Alien`~scale(total_score)+I(scale(total_score)^2),disp=~scale(total_score)+I(scale(total_score)^2),data=country_record_score))
+summary(glmmTMB(`2°C Alien`~scale(total_score)+I(scale(total_score)^2),disp=~scale(total_score)+I(scale(total_score)^2),data=country_record_score))
+summary(glmmTMB(`2°C Invasive`~scale(total_score)+I(scale(total_score)^2),disp=~scale(total_score)+I(scale(total_score)^2),data=country_record_score))
+summary(glmmTMB(`4°C Alien`~scale(total_score)+I(scale(total_score)^2),disp=~scale(total_score)+I(scale(total_score)^2),data=country_record_score))
+summary(glmmTMB(`4°C Invasive`~scale(total_score)+I(scale(total_score)^2),disp=~scale(total_score)+I(scale(total_score)^2),data=country_record_score))
+
+
 
 #####################################################################################################################
 country_score <- sp_country %>% group_by(Final_country) %>% summarise("2°C Alien" = sum(proj_diff_2C),
@@ -59,7 +100,7 @@ for (num in 1:4){
                                        ylab("")+
                                        xlab("")+
                                        labs(fill="Score")+
-                                       scale_fill_manual(values=c("blue","purple","red"))+
+                                       scale_fill_manual(values=c("#ffffb2","#fd8d3c","#bd0026"))+
                                        ggtitle(var[[num]])+
                                        theme_classic()+
                                        theme(axis.text.x = if(num==1){element_text(angle = 10,size=6)}else{element_text(angle=10,colour = "white",size=6)}))
--- ant_formal_analysis.R
+++ ant_formal_analysis.R
@@ -17,9 +17,11 @@ NMI_analysis <- subset(clim_invasion_df)
 #NMI_analysis <- NMI_analysis[NMI_analysis$species %in% indoor_sp,]
 #NMI_analysis <- na.omit(NMI_analysis)
 
-ant_tree <- read.tree("C:/Users/pakno/OneDrive - University of Toronto/ant_tree/backbone_MLtree_RaxML.tre")
+ant_tree <- org_ant_tree <- read.tree("C:/Users/pakno/OneDrive - University of Toronto/ant_tree/backbone_MLtree_RaxML.tre")
 is.ultrametric(ant_tree)
-ant_tree <- chronos(ant_tree,lambda=0)
+#ant_tree <- force.ultrametric(ant_tree,method="nnls")
+ant_tree <- as.phylo(hclust(as.dist(cophenetic(ant_tree)),method="average"))
+dendextend::cor_cophenetic(ant_tree,org_ant_tree)
 ant_tree$node.label <- 1:length(ant_tree$node.label)
 
 ant_tree$tip.label <- gsub("[\"]","",ant_tree$tip.label)
@@ -50,44 +52,10 @@ NMI_analysis$num <- ifelse(NMI_analysis$num >0, 1,0)
 #####################Characterize environments
 options(glmmTMB.cores=6)
 NMI_analysis$factor <- as.factor(NMI_analysis$num)
-library(glmmTMB)
-
-m_MAT <- lm((NMI_analysis$MAT-NMI_analysis$niche.MAT)~factor,data=NMI_analysis)
-summary(m_MAT)
-car::Anova(m_MAT,white.adjust=T)
-
-m_Temp_sd <-lm((NMI_analysis$Temp_sd-NMI_analysis$niche.Temp_sd)~factor,data=NMI_analysis)
-summary(m_Temp_sd)
-car::Anova(m_Temp_sd,white.adjust=T)
-
-m_Prec <- lm((NMI_analysis$Prec-NMI_analysis$niche.Prec)~factor,data=NMI_analysis)
-summary(m_Prec)
-car::Anova(m_Prec,white.adjust=T)
-
-m_Prec_sd <-lm((NMI_analysis$Prec_sd-NMI_analysis$niche.Prec_sd)~factor,data=NMI_analysis)
-summary(m_Prec_sd)
-car::Anova(m_Prec_sd,white.adjust=T)
-
-m_SM <- lm((NMI_analysis$SM-NMI_analysis$niche.SM)~factor,data=NMI_analysis)
-summary(m_SM)
-car::Anova(m_SM,white.adjust=T)
-
-m_SM_sd <-lm((NMI_analysis$SM_sd-NMI_analysis$niche.SM_sd)~factor,data=NMI_analysis)
-summary(m_SM_sd)
-car::Anova(m_SM_sd,white.adjust=T)
 
 library(glmmTMB)
-NMI_analysis$Soil <- 0
-NMI_analysis$Soil[NMI_analysis$sp.layer == "Soil"] <- 1
-NMI_analysis$Generalist <- 0
-NMI_analysis$Generalist[NMI_analysis$sp.layer == "Generalist"] <- 1
-NMI_analysis$Arboreal<- 0
-NMI_analysis$Arboreal[NMI_analysis$sp.layer == "Arboreal"] <- 1
-NMI_analysis$Soil <- scale(NMI_analysis$Soil,center=T,scale=F)
-NMI_analysis$Generalist <- scale(NMI_analysis$Generalist,center=T,scale=F)
-NMI_analysis$Arboreal <- scale(NMI_analysis$Arboreal,center=T,scale=F)
 
-m <- glmmTMB(num~scaled.water_pca*scaled.niche_water_pca+scaled.temp_pca*scaled.niche_temp_pca+scaled.date+Generalist+Arboreal+(scaled.water_pca+scaled.temp_pca||species)+(scaled.niche_water_pca+scaled.niche_temp_pca||polygon_name),
+m <- glmmTMB(num~scaled.water_pca*scaled.niche_water_pca+scaled.temp_pca*scaled.niche_temp_pca+scaled.date+sp.layer+(scaled.water_pca+scaled.temp_pca||species)+(scaled.niche_water_pca+scaled.niche_temp_pca||polygon_name),
              data=NMI_analysis,family="binomial")
 summary(m)
 car::Anova(m)
@@ -103,15 +71,23 @@ write.csv(round(summary(m)$coef$cond,3),"glmm.csv")
 
 library(DHARMa)
 library(geosphere)
-sres <- simulateResiduals(m)
-sres_spatial <- recalculateResiduals(sres,NMI_analysis$polygon_name)
-polygon_unique <- NMI_analysis[!duplicated(NMI_analysis$polygon_name),c("polygon_name","x","y")]
+m1 <- glmmTMB(num~scaled.water_pca*scaled.niche_water_pca+scaled.temp_pca*scaled.niche_temp_pca+scaled.date+sp.layer+(scaled.water_pca+scaled.temp_pca||species)+(scaled.niche_water_pca+scaled.niche_temp_pca||polygon_name),
+             data=NMI_analysis[order(NMI_analysis$polygon_name),],family="binomial") #re-run model but make sure the factors are ordered alphabetically
+summary(m1)
+
+sres <- simulateResiduals(m1)
+sres_spatial <- recalculateResiduals(sres,NMI_analysis[order(NMI_analysis$polygon_name),"polygon_name"])
+polygon_unique <- NMI_analysis[!duplicated(NMI_analysis$polygon_name),c("polygon_name","x","y")] #make sure the structures are consistent between residual objects and distance M
 polygon_unique <- polygon_unique[order(polygon_unique$polygon_name),]
 polygon_dist <- distm(polygon_unique[,c("x","y")])
 testSpatialAutocorrelation(sres_spatial,distMat=polygon_dist)
 
+m2 <- glmmTMB(num~scaled.water_pca*scaled.niche_water_pca+scaled.temp_pca*scaled.niche_temp_pca+scaled.date+sp.layer+(scaled.water_pca+scaled.temp_pca||species)+(scaled.niche_water_pca+scaled.niche_temp_pca||polygon_name),
+              data=NMI_analysis[order(NMI_analysis$species),],family="binomial")
+summary(m2) #re-run model but make sure the factors are ordered alphabetically
+sres <- simulateResiduals(m2)
 phyD <- cophenetic(keep.tip(genus_tree,NMI_analysis$species))
-sres_phylo <- recalculateResiduals(sres,NMI_analysis$species)
+sres_phylo <- recalculateResiduals(sres,NMI_analysis[order(NMI_analysis$species),"species"])
 phyD <- phyD[order(rownames(phyD)),order(colnames(phyD))]
 testSpatialAutocorrelation(sres_phylo,distMat=phyD)
 ###################################################Fig 2
@@ -144,7 +120,7 @@ p2b<- ggplot(predict_niche,aes(y=group,x=x))+
   annotate("text", x = -Inf, y = Inf, hjust=0,vjust=1,label = "(b)",size=3)+
   xlab("\n")+
   ylab("Temperature PCA1 of native range \n (Colder and more seasonal)")+
-  scale_fill_continuous(low="blue",high="red",breaks=c(0.1,0.5,0.9))+
+  scale_fill_continuous(low="#ffffb2",high="#e31a1c",breaks=c(0.1,0.5,0.9))+
   scale_color_manual(values=c("grey75","black"))+
   theme
 
@@ -165,7 +141,7 @@ p2a <- ggplot(predict_water,aes(y=group,x=x))+
   annotate("text", x = -Inf, y = Inf, hjust=0,vjust=1,label = "(a)",size=3)+
   xlab("Temperature PCA1 of invaded region \n (Colder and more seasonal)")+
   ylab("Water PCA1 of invaded region \n (Wetter and more seasonal)")+
-  scale_fill_continuous(low="blue",high="red",name="Predicted probability of establishing in the wild",breaks=c(0.1,0.5,0.9))+
+  scale_fill_continuous(low="#ffffb2",high="#e31a1c",name="Predicted probability of establishing in the wild",breaks=c(0.1,0.5,0.9))+
   scale_color_continuous(low="grey75",high="black",name="Proportion of species established in the wild",breaks=c(0.1,0.5,0.9))+
   theme
 
@@ -248,7 +224,7 @@ not_found <- sort(unique(not_found))
 NMI_analysis$dummy <- 1
 ######################################################
 
-future_df <- NMI_analysis[,c("species","polygon_name","shp.ID","sp.layer","Generalist","Arboreal","scaled.date","scaled.niche_temp_pca","scaled.niche_water_pca","dummy")]
+future_df <- NMI_analysis[,c("species","polygon_name","shp.ID","sp.layer","scaled.date","scaled.niche_temp_pca","scaled.niche_water_pca","dummy")]
 
 source("Indoor_ants/Indoor_ants/rescale_future_climate.R")
 NMI_analysis$scaled.temp_pca_2C <- rescale_future_climate(NMI_analysis[,c("MAT.2C","Temp_sd.2C")],
@@ -270,10 +246,10 @@ NMI_analysis$scaled.water_pca_4C<- rescale_future_climate(NMI_analysis[,c("Prec.
 
 NMI_analysis$current_status_projection <- predict(m,type="response")
 
-predict_data <- list(NMI_analysis[,c("species","polygon_name","scaled.date","scaled.niche_temp_pca","scaled.niche_water_pca","scaled.temp_pca_2C","scaled.water_pca_2C","Arboreal","Generalist")],
-                     NMI_analysis[,c("species","polygon_name","scaled.date","scaled.niche_temp_pca","scaled.niche_water_pca","scaled.temp_pca_4C","scaled.water_pca_4C","Arboreal","Generalist")])
+predict_data <- list(NMI_analysis[,c("species","polygon_name","scaled.date","scaled.niche_temp_pca","scaled.niche_water_pca","scaled.temp_pca_2C","scaled.water_pca_2C","sp.layer")],
+                     NMI_analysis[,c("species","polygon_name","scaled.date","scaled.niche_temp_pca","scaled.niche_water_pca","scaled.temp_pca_4C","scaled.water_pca_4C","sp.layer")])
 
-predict_data <- lapply(predict_data,setNames,c("species","polygon_name","scaled.date","scaled.niche_temp_pca","scaled.niche_water_pca","scaled.temp_pca","scaled.water_pca","Arboreal","Generalist"))
+predict_data <- lapply(predict_data,setNames,c("species","polygon_name","scaled.date","scaled.niche_temp_pca","scaled.niche_water_pca","scaled.temp_pca","scaled.water_pca","sp.layer"))
 
 future_occ <- do.call(cbind,lapply(predict_data,function(x) predict(m,x,type="response")))
 colnames(future_occ) <- c("future_status_2C","future_status_4C")
@@ -297,6 +273,7 @@ NMI_analysis$warming_diff <- NMI_analysis$future_status_4C-NMI_analysis$future_s
 site_summary <- NMI_analysis %>% group_by(polygon_name,shp.ID) %>% summarize(future_sum_2C=sum(future_status_2C),
                                                                           future_sum_4C=sum(future_status_4C),
                                                                           current_sum = sum(num),
+                                                                          current_indoor = sum(dummy)-sum(num),
                                                                           current_projection_sum = sum(current_status_projection),
                                                                           proj_diff_2C_net = sum(proj_diff_2C),
                                                                           proj_diff_4C_net = sum(proj_diff_4C),
@@ -371,7 +348,7 @@ p1a <- ggplot(data=bentity.df,aes(x=x,y=y,group=id,fill=indoor.sr))+
   annotate("text", x = -Inf, y = Inf, hjust=0,vjust=1,label = "(a) Alien")+
   annotation_custom(g1, xmin=-175, xmax=-175, ymin=-55, ymax=-55) +
   labs(fill="")+
-  scale_fill_continuous(low="blue",high="red",na.value="white",limits=c(0,71),breaks=c(1,35,70))+
+  scale_fill_continuous(low="#ffffb2",high="#bd0026",na.value="white",limits=c(0,70),breaks=c(1,35,70))+
   theme
 
 p1b <- ggplot(data=bentity.df,aes(x=x,y=y,group=id,fill=outdoor.sr))+
@@ -379,7 +356,7 @@ p1b <- ggplot(data=bentity.df,aes(x=x,y=y,group=id,fill=outdoor.sr))+
   annotate("text", x = -Inf, y = Inf, hjust=0,vjust=1,label = "(b) Alien")+
   annotation_custom(g2, xmin=-175, xmax=-175, ymin=-55, ymax=-55) +
   labs(fill="")+
-  scale_fill_continuous(low="blue",high="red",na.value="white",limits=c(0,71),breaks=c(1,35,70))+
+  scale_fill_continuous(low="#ffffb2",high="#bd0026",na.value="white",tran=scales::log1p_trans(),limits=c(0,70),breaks=c(1,35,70))+
   theme
 
 p1c <- ggplot(data=bentity.df,aes(x=x,y=y,group=id,fill=indoor.sr.invasive))+
@@ -387,7 +364,7 @@ p1c <- ggplot(data=bentity.df,aes(x=x,y=y,group=id,fill=indoor.sr.invasive))+
   annotate("text", x = -Inf, y = Inf, hjust=0,vjust=1,label = "(c) Invasive")+
   annotation_custom(g1, xmin=-175, xmax=-175, ymin=-55, ymax=-55) +
   labs(fill="")+
-  scale_fill_continuous(low="blue",high="red",na.value="white",limits=c(0.5,12.5),breaks=c(1,6,12))+
+  scale_fill_continuous(low="#ffffb2",high="#bd0026",na.value="white",limits=c(0.5,12.5),breaks=c(1,6,12))+
   theme
 
 p1d <- ggplot(data=bentity.df,aes(x=x,y=y,group=id,fill=outdoor.sr.invasive))+
@@ -395,7 +372,7 @@ p1d <- ggplot(data=bentity.df,aes(x=x,y=y,group=id,fill=outdoor.sr.invasive))+
   annotate("text", x = -Inf, y = Inf, hjust=0,vjust=1,label = "(d) Invasive")+
   annotation_custom(g2, xmin=-175, xmax=-175, ymin=-55, ymax=-55) +
   labs(fill="")+
-  scale_fill_continuous(low="blue",high="red",na.value="white",limits=c(0.5,12.5),breaks=c(1,6,12))+
+  scale_fill_continuous(low="#ffffb2",high="#bd0026",na.value="white",limits=c(0.5,12.5),breaks=c(1,6,12))+
   theme
 
 library(ggpubr)
@@ -446,7 +423,7 @@ p3a <- ggplot(data=bentity.df,aes(x=x,y=y,group=id,fill=future_gain_2C))+
   annotate("text", x = -170, y = -55,label = "2°C",colour="red",size=4,hjust=0)+
   annotation_custom(g3, xmin=-180, xmax=-180, ymin=-55, ymax=-55) +
   labs(fill="")+
-  scale_fill_continuous(low="blue",high="red",na.value="white",limits=c(-0.1,9),breaks = c(0,2,4,6,8))+
+  scale_fill_continuous(low="#ffffb2",high="#bd0026",na.value="white",limits=c(-0.1,9),breaks = c(0,2,4,6,8))+
   theme
 plot(p3a)
 
@@ -456,7 +433,7 @@ p3b <- ggplot(data=bentity.df,aes(x=x,y=y,group=id,fill=future_gain_4C))+
   annotate("text", x = -170, y = -55,label = "4°C",colour="red",hjust=0)+
   annotation_custom(g3, xmin=-180, xmax=-180, ymin=-55, ymax=-55) +
   labs(fill="")+
-  scale_fill_continuous(low="blue",high="red",na.value="white",limits=c(-0.1,9),breaks = c(0,2,4,6,8))+
+  scale_fill_continuous(low="#ffffb2",high="#bd0026",na.value="white",limits=c(-0.1,9),breaks = c(0,2,4,6,8))+
   theme
 plot(p3b)
 
@@ -466,7 +443,7 @@ p3c <- ggplot(data=bentity.df,aes(x=x,y=y,group=id,fill=future_gain_invasive_2C)
   annotate("text", x = -170, y = -55,label = "2°C",colour="red",hjust=0)+
   annotation_custom(g3, xmin=-180, xmax=-180, ymin=-55, ymax=-55) +
   labs(fill="")+
-  scale_fill_continuous(low="blue",high="red",na.value="white",limits=c(-0.1,3.2),breaks = c(0,1,2,3))+
+  scale_fill_continuous(low="#ffffb2",high="#bd0026",na.value="white",limits=c(-0.1,3.2),breaks = c(0,1,2,3))+
   theme
 plot(p3c)
 
@@ -476,7 +453,7 @@ p3d <- ggplot(data=bentity.df,aes(x=x,y=y,group=id,fill=future_gain_invasive_4C)
   annotate("text", x = -170, y = -55,label = "4°C",colour="red",hjust=0)+
   annotation_custom(g3, xmin=-180, xmax=-180, ymin=-55, ymax=-55) +
   labs(fill="")+
-  scale_fill_continuous(low="blue",high="red",na.value="white",limits=c(-0.1,3.2),breaks = c(0,1,2,3))+
+  scale_fill_continuous(low="#ffffb2",high="#bd0026",na.value="white",limits=c(-0.1,3.2),breaks = c(0,1,2,3))+
   theme
 plot(p3d)
 
@@ -486,7 +463,7 @@ p3e <- ggplot(data=bentity.df,aes(x=x,y=y,group=id,fill=warming_diff))+
   annotate("text", x = -170, y = -55,label = "4°C vs 2°C",colour="red",hjust=0)+
   annotation_custom(g3, xmin=-180, xmax=-180, ymin=-55, ymax=-55) +
   labs(fill="")+
-  scale_fill_continuous(low="blue",high="red",na.value="white",limits=c(-0.1,6.1),breaks = c(0,2,4,6))+
+  scale_fill_continuous(low="#ffffb2",high="#bd0026",na.value="white",limits=c(-0.1,6.1),breaks = c(0,2,4,6))+
   theme
 plot(p3e)
 
@@ -496,7 +473,7 @@ p3f <- ggplot(data=bentity.df,aes(x=x,y=y,group=id,fill=warming_diff_invasive))+
   annotate("text", x = -170, y = -55,label = "4°C vs 2°C",colour="red",hjust=0)+
   annotation_custom(g3, xmin=-180, xmax=-180, ymin=-55, ymax=-55) +
   labs(fill="")+
-  scale_fill_continuous(low="blue",high="red",na.value="white",limits=c(-0.1,2),breaks = c(0,0.5,1,1.5))+
+  scale_fill_continuous(low="#ffffb2",high="#bd0026",na.value="white",limits=c(-0.1,2),breaks = c(0,0.5,1,1.5))+
   theme
 plot(p3f)
 
@@ -506,8 +483,15 @@ plot(p3)
 
 ggsave("Indoor_ants/Fig3.tiff",dpi=800,compression="lzw",units="cm",height=16.8,width=16.8,bg="white")
 
+
+# have to mean them first rather than calculating percent change in each region before averaging - because some regions have inf values (0 invasive currently at outdoor)
+
+site_summary %>% filter(current_indoor >0) %>% reframe(percent_2C = mean(proj_diff_2C_net)/mean(current_projection_sum)*100,
+                                                       percent_4C = mean(proj_diff_4C_net)/mean(current_projection_sum)*100,
+                                                       percent_2C_invasive = mean(proj_diff_invasive_2C_net)/mean(current_invasive_sum)*100,
+                                                       percent_4C_invasive = mean(proj_diff_invasive_4C_net)/mean(current_invasive_sum)*100) 
 #################previous clunky code. no longer useful
-future_df <- NMI_analysis[,c("species","polygon_name","shp.ID","sp.layer","Generalist","Arboreal","scaled.date","scaled.niche_temp_pca","scaled.niche_water_pca","dummy")]
+future_df <- NMI_analysis[,c("species","polygon_name","shp.ID","sp.layer","scaled.date","scaled.niche_temp_pca","scaled.niche_water_pca","dummy")]
 future_temp <- t(apply(NMI_analysis[,c("MAT.2C","Temp_sd.2C")],1, function(x) (x-colMeans(NMI_analysis[,c("MAT","Temp_sd")]))/apply(NMI_analysis[,c("MAT","Temp_sd")],2,sd)))
 colnames(future_temp) <- c("MAT","Temp_sd")
 future_df$temp_pca <- apply(future_temp,1, function(x) rowSums(x*t(NMI_analysis_pca_temp$rotation[,1])))
@@ -616,7 +600,7 @@ Country_summary_2C_invasive <- site_summary_invasive %>% group_by(Final_country)
 ##################### 4C Projection
 
 NMI_analysis$dummy <- 1
-future_df <- NMI_analysis[,c("species","polygon_name","shp.ID","sp.layer","Generalist","Arboreal","scaled.date","scaled.niche_temp_pca","scaled.niche_water_pca","dummy")]
+future_df <- NMI_analysis[,c("species","polygon_name","shp.ID","sp.layer","scaled.date","scaled.niche_temp_pca","scaled.niche_water_pca","dummy")]
 future_temp <- t(apply(NMI_analysis[,c("MAT.4C","Temp_sd.4C")],1, function(x) (x-colMeans(NMI_analysis[,c("MAT","Temp_sd")]))/apply(NMI_analysis[,c("MAT","Temp_sd")],2,sd)))
 colnames(future_temp) <- c("MAT","Temp_sd")
 future_df$temp_pca <- apply(future_temp,1, function(x) rowSums(x*t(NMI_analysis_pca_temp$rotation[,1])))
